c1cc21c02797d09d2c7f03edbd5d4295af33358c
Willem van Bergen <willem@vanbergen.org>
2020-12-18 15:56:19 -0500

    Test

--- transport.go
+++ transport.go
@@ -103,7 +103,7 @@ func (t *Transport) RoundTrip(req *http.Request) (*http.Response, error) {
 	}
 
 	req.Header.Set("Authorization", "token "+token)
-	req.Header.Add("Accept", acceptHeader) // We add to "Accept" header to avoid overwriting existing req headers.
+//	req.Header.Add("Accept", acceptHeader) // We add to "Accept" header to avoid overwriting existing req headers.
 	resp, err := t.tr.RoundTrip(req)
 	return resp, err
 }
